e041e6a7d8005449942680cd958607a1546b8bba
Adam Carroll <Oceloteater@users.noreply.github.com>
2019-07-11 23:01:49 +0200

    Refactoring and adding page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,23 @@
-import React, { Fragment, useContext, useEffect } from 'react';
+import React from 'react';
 import { Store } from './Store';
+import { Link } from '@reach/router';
 
-const EpisodesList = React.lazy(() => import('./EpisodesList'));
-
-export default function App() {
-    const { state, dispatch } = useContext(Store);
-
-    const fetchDataAction = async () => {
-        const data = await fetch('https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes');
-        const dataJSON = await data.json();
-        return dispatch({
-            type: 'FETCH_DATA',
-            payload: dataJSON._embedded.episodes
-        });
-    };
-
-    const toggleFavAction = episode => {
-        const episodeInFavourites = state.favourites.includes(episode);
-        let dispatchObj = {
-            type: 'ADD_FAV',
-            payload: episode
-        };
-        if (episodeInFavourites) {
-            const favouritesWithoutEpisode = state.favourites.filter(fav => fav.id !== episode.id);
-            dispatchObj = {
-                type: 'REMOVE_FAV',
-                payload: favouritesWithoutEpisode
-            };
-        }
-        return dispatch(dispatchObj);
-    };
-
-    const props = {
-        episodes: state.episodes,
-        toggleFavAction: toggleFavAction,
-        favourites: state.favourites
-    };
-
-    useEffect(() => {
-        state.episodes.length === 0 && fetchDataAction();
-    });
+export default function App(props) {
+    const { state } = React.useContext(Store);
 
     return (
-
-        <Fragment>
-            <React.Suspense fallback={<div>Loading...</div>}>
-                {console.log(state, dispatch)}
+        <React.Fragment>
+            <header className='header'>
                 <div>
-                    <header className='header'>
-                        <h1>Rick and Morty</h1>
-                        <p>Pick your favourite episodes</p>
-                        <div>Favourite(s) {state.favourites.length}</div>
-                    </header>
-                    <section className='episode-layout'>
-                        <EpisodesList {...props} />
-                    </section>
+                    <h1>Rick and Morty</h1>
+                    <p>Pick your favourite episodes</p>
                 </div>
-            </React.Suspense>
-        </Fragment>
+                <div>
+                    <Link to='/'>Home</Link>{' '}
+                    <Link to='/faves'>Favourite(s) {state.favourites.length}</Link>
+                </div>
+            </header>
+            {props.children}
+        </React.Fragment>
     );
 }
