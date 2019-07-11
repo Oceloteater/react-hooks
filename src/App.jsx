91ecf48f9711a03dce6500cc275203f8af86bbcb
Adam Carroll <Oceloteater@users.noreply.github.com>
2019-07-11 22:19:13 +0200

    Implementing fav and unfav toggle logic

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,37 @@ export default function App() {
         });
     };
 
+    const toggleFavAction = episode => {
+        const episodeInFavourites = state.favourites.includes(episode);
+        let dispatchObj = {
+            type: 'ADD_FAV',
+            payload: episode
+        };
+        if (episodeInFavourites) {
+            const favouritesWithoutEpisode = state.favourites.filter(fav => fav.id !== episode.id);
+            dispatchObj = {
+                type: 'REMOVE_FAV',
+                payload: favouritesWithoutEpisode
+            };
+        }
+        return dispatch(dispatchObj);
+    };
+
     useEffect(() => {
-        state.episodes.length === 0 &&
-        fetchDataAction();
+        state.episodes.length === 0 && fetchDataAction();
     });
 
     return (
         <Fragment>
             {console.log(state, dispatch)}
             <div>
-                <h1>Rick and Morty</h1>
-                <p>Pick your favourite episodes</p>
-                <section>
+                <header className='header'>
+                    <h1>Rick and Morty</h1>
+                    <p>Pick your favourite episodes</p>
+                    <div>Favourite(s) {state.favourites.length}</div>
+                </header>
+
+                <section className='episode-layout'>
                     {state.episodes.map(episode => {
                         return (
                             <section key={episode.id}>
@@ -33,10 +52,13 @@ export default function App() {
                                     alt={`Rick and Morty ${episode.name}`}
                                 />
                                 <div>{episode.name}</div>
-                                <section>
+                                <section style={{ display: 'flex', justifyContent: 'space-between' }}>
                                     <div>
                                         Season: {episode.season} Number: {episode.number}
                                     </div>
+                                    <button type='button' onClick={() => toggleFavAction(episode)}>
+                                        {state.favourites.find(fav => fav.id === episode.id) ? 'Unfav' : 'Fav'}
+                                    </button>
                                 </section>
                             </section>
                         );
